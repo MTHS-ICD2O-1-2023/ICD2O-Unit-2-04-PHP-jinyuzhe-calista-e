451122bc462ab808b813dfdafb81f29e120fa5ca
Calista.E <159185314+jinyuzhe-calista-e@users.noreply.github.com>
2024-03-22 18:22:27 +0000

    done

--- answer.php
+++ answer.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8" />
+     <meta charset="utf-8" />
     <meta name="description" content="Area of Triangle, in PHP" />
     <meta name="keywords" content="mths, icd2o" />
     <meta name="author" content="Calista.E" />
@@ -13,7 +13,7 @@
     <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
     <link rel="manifest" href="site.webmanifest" />
-    <title>Area and of Triangle,in PHP</title>
+    <title>Area of Triangle,in PHP</title>
   </head>
   <body>
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
@@ -27,27 +27,22 @@
         <div class="right-image">
           <img src="./image/area_of_trinagle.png" alt="laptop image" width="250" />
         </div>
-        <br />
-        <div class="page-content">Enter the base and height in cm.</div>
         <div class="page-content-php">
-          <form action="answer.php" method="POST">
-            <div class="mdl-textfield mdl-js-textfield">
-              <input class="mdl-textfield__input" type=" text" pattern="-?[0-9]*(\.[0-9]+)?" name="base-of-triangle">
-              <label class="mdl-textfield__label" for="base-of-triangle">Base of triangle (cm)</label>
-              <span class="mdl-textfield__error">Input is not a number!</span>
-            </div>
-            <br />
-            <div class="mdl-textfield mdl-js-textfield">
-              <input class="mdl-textfield__input" type=" text" pattern="-?[0-9]*(\.[0-9]+)?" name="height-of-triangle">
-              <label class="mdl-textfield__label" for="height-of-triangle">Height of triangle (cm)</label>
-              <span class="mdl-textfield__error">Input is not a number!</span>
-            </div>
-            <br />
-            <!-- Accent-colored raised button with ripple -->
-            <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit">
-              Calculate
-             </button>
-          </form>
+          <div id="user-info">
+            <?php
+            $baseOfRectangle = $_POST["base-of-rectangle"];
+            $heightOfRectangle = $_POST["height-of-rectangle"];
+
+            // process
+            $area = $baseOfRectangle * $heightOfRectangle * 0.5;
+
+            // output
+            echo "If a triangle has base = " . $baseOfRectangle . " cm and the height = " . $heightOfRectangle . " cm:";
+            echo "<br />";
+            echo "<br />";
+            echo "The area of the triangle is " . $area . " cm².";
+            ?>
+          </div>
         </div>
       </main>
     </div>
